40814e6187a13659908e4fd6a8a76657c5119a05
Jason Behrend <jasonbehrend@jasons-macbook-air.local>
2016-02-01 15:46:41 -0800

    Initial Commit

---
+++ Baseball Calculator 2.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Baseball Calculator 2.0
+//
+//  Created by Jason Behrend on 2/1/16.
+//  Copyright © 2016 JasonBehrend. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
