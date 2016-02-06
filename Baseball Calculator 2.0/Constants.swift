a88ac0aebe52202ceddddd51fb79b31f8ba0f9c7
Jason Behrend <jasonbehrend@Jasons-Air.attlocal.net>
2016-02-05 18:11:03 -0800

    Started stats screen view

---
+++ Baseball Calculator 2.0/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Baseball Calculator 2.0
+//
+//  Created by Jason Behrend on 2/5/16.
+//  Copyright © 2016 JasonBehrend. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SHADOW_COLOR: CGFloat = 157.0 / 255.0
